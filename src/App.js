f20cbe4de97d25a919091ecf9c415d7758923e04
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-11-29 16:06:56 -0300

    comeco

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <h1>Vamo timee!</h1>
   );
 }
 
