aff1afb701b91c057d867130fe81464ecb2e8467
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-11-29 18:20:31 -0300

    inicio requisito 7

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function DoneRecipes() {
   return (
-    <h1>DoneRecipes</h1>
+    <Header />
   );
 }
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Profile() {
   return (
-    <h1>Profile</h1>
+    <Header />
   );
 }
 
