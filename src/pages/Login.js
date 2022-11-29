ed9d061e74c4d19711b78873ef50495b65aa989f
jaidernunes <jaidernf@gmail.com>
2022-11-29 20:07:28 -0300

    Req 5 & 6: Pass! - Implemented Login button handler

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Login() {
   const [loginValidator, setLoginValidator ] = useState('disabled');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     const handleLoginValidator = () => {
@@ -21,15 +22,23 @@ function Login() {
     handleLoginValidator()
   }, [email, password]);
 
+  const handleLogin = () => {
+    localStorage.setItem('user', JSON.stringify({email: email}));
+    history.push("/meals");
+  };
+
   return (
     <>
       <h1>Login</h1>
       <form action="">
         <input type="text" name="emailInput" id="emailInput" data-testid="email-input" 
         onChange={({target}) => setEmail(target.value) }/>
+
         <input type="password" name="passwordInput" id="passwordInput" data-testid="password-input"
         onChange={({target}) => setPassword(target.value) }/>
-        <button name="loginSubmitBtn" data-testid="login-submit-btn" disabled={loginValidator}
+        
+        <button name="loginSubmitBtn" data-testid="login-submit-btn" 
+        disabled={loginValidator} onClick={handleLogin}
         >ENTER</button>
       </form>
     </>
