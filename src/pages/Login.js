874af7e0b19ec35c4ec6878d8c35be050321dc54
Joana <joanamdsantos1@gmail.com>
2022-11-30 11:00:44 -0300

    lint corrigido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 function Login() {
-  const [loginValidator, setLoginValidator ] = useState('disabled');
+  const [loginValidator, setLoginValidator] = useState('disabled');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
@@ -10,8 +10,8 @@ function Login() {
   useEffect(() => {
     const handleLoginValidator = () => {
       const minimumPassword = 6;
-      const emailValidator = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-      
+      const emailValidator = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+
       if (emailValidator.test(email) && password.length > minimumPassword) {
         setLoginValidator(false);
       } else {
@@ -19,30 +19,45 @@ function Login() {
       }
     };
 
-    handleLoginValidator()
+    handleLoginValidator();
   }, [email, password]);
 
   const handleLogin = () => {
-    localStorage.setItem('user', JSON.stringify({email: email}));
-    history.push("/meals");
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/meals');
   };
 
   return (
     <>
       <h1>Login</h1>
       <form action="">
-        <input type="text" name="emailInput" id="emailInput" data-testid="email-input" 
-        onChange={({target}) => setEmail(target.value) }/>
+        <input
+          type="text"
+          name="emailInput"
+          id="emailInput"
+          data-testid="email-input"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
 
-        <input type="password" name="passwordInput" id="passwordInput" data-testid="password-input"
-        onChange={({target}) => setPassword(target.value) }/>
-        
-        <button name="loginSubmitBtn" data-testid="login-submit-btn" 
-        disabled={loginValidator} onClick={handleLogin}
-        >ENTER</button>
+        <input
+          type="password"
+          name="passwordInput"
+          id="passwordInput"
+          data-testid="password-input"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+        <button
+          type="button"
+          name="loginSubmitBtn"
+          data-testid="login-submit-btn"
+          disabled={ loginValidator }
+          onClick={ handleLogin }
+        >
+          ENTER
+        </button>
       </form>
     </>
   );
-};
+}
 
 export default Login;
