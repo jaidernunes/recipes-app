e81339d5f49a35d54a9df19e2a42279823669ae7
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-11-30 16:32:30 -0300

    testes requisitos 7, 8, 9

---
+++ src/tests/Header.test.js
@@ -0,0 +1,14 @@
+import { act, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('<Header />', () => {
+  test('se aperece o titulo Meals na rota /meals', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/meals');
+    });
+
+    const title = screen.getAllByText('Meals');
+  });
+});
