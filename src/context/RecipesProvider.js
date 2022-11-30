b4c1194e9af351d44a098e4d26c5d83cc958ab08
IgorSouzaDeOliveira <igorsouzaoliveira002@gmail.com>
2022-11-30 17:52:40 -0300

    req 19 - add 12 primeirasreceitas

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,14 +1,36 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
 function RecipesProvider({ children }) {
   const [teste, setTeste] = useState([]);
 
+  // estado da primeira requisição
+  const [mealsRequest, setMealsRequest] = useState([]);
+  const [drinksRequest, setDrinksRequest] = useState([]);
+
+  useEffect(() => {
+    const requestMeals = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const result = await response.json();
+      setMealsRequest(result.meals);
+    };
+
+    const requestDrinks = async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const result = await response.json();
+      setDrinksRequest(result.drinks);
+    };
+    requestMeals();
+    requestDrinks();
+  }, []);
+
   const providerProps = useMemo(() => ({
     teste,
     setTeste,
-  }), [teste,
+    mealsRequest,
+    drinksRequest,
+  }), [teste, drinksRequest, mealsRequest,
   ]);
 
   return (
