68fb29102481f411060798037aefc62c3cc759db
Priscila <priscilasartori2@gmail.com>
2022-11-30 17:22:57 -0300

    Acerto de arquivo release

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,8 +10,7 @@ function Login() {
   useEffect(() => {
     const handleLoginValidator = () => {
       const minimumPassword = 6;
-      const emailValidator = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
+      const emailValidator = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
       if (emailValidator.test(email) && password.length > minimumPassword) {
         setLoginValidator(false);
       } else {
@@ -47,6 +46,7 @@ function Login() {
           onChange={ ({ target }) => setPassword(target.value) }
         />
         <button
+          type="button"
           name="loginSubmitBtn"
           data-testid="login-submit-btn"
           disabled={ loginValidator }
