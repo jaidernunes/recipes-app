86f23cba2c906b9336e95ecc620a2811e5e6d36f
jaidernunes <jaidernf@gmail.com>
2022-11-30 18:05:36 -0300

    Login page and tests done - Router moved to index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Routes />
   );
 }
 
