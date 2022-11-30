081330767aaebe2ca4fd258574521e6d5ca21e02
Joana <joanamdsantos1@gmail.com>
2022-11-30 17:56:23 -0300

    modificando a requisição da API

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 // import { useParams } from 'react-router-dom';
 import { getMealDetails } from '../services/detailsAPI';
 
 function RecipeDetails() {
   // const { id } = useParams();
-  const { match } = useHistory();
-  const [recipe, setRecipes] = useState([]);
+  // const { match } = useHistory();
+  const [recipe, setRecipe] = useState([]);
 
   const fetchMeal = async () => {
     const recipeId = 52772;
     const getMeal = await getMealDetails(recipeId);
-    // const [meals] = setMeal;
-    setRecipes(getMeal);
+    console.log(getMeal);
+    setRecipe(getMeal[0].strlMeal);
+    console.log(recipe);
   };
 
   useEffect(() => {
     fetchMeal();
-    console.log(recipe);
-    console.log(match);
   }, []);
 
   return (
-    // <h1>Detalhes</h1>
-    <h1>{ recipe.meals[0].strMeal }</h1>
+    <h1>Receita</h1>
+    // <h1>
+    //   {`Receita de ${recipe[0].strMeal}`}
+    // </h1>
   );
 }
 
--- src/services/detailsAPI.js
+++ src/services/detailsAPI.js
@@ -1,11 +1,12 @@
 export const getMealDetails = async (mealId) => {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
-  const json = response.json();
-  return json;
+  const json = await response.json();
+  const { meals } = json;
+  return meals;
 };
 
 export const getCocktailDetails = async (cocktailId) => {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`);
-  const json = response.json();
+  const json = await response.json();
   return json;
 };
