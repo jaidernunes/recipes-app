d5f90eb421ec6e362a28b83d81ec301ff38c56da
Priscila <priscilasartori2@gmail.com>
2022-12-13 18:29:42 -0300

    Requisito 16 e 18

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 function Footer() {
   const history = useHistory();
   return (
-    <div data-testid="footer">
+    <div data-testid="footer" className="footer">
       <button
         type="button"
         onClick={ () => history.push('/drinks') }
+        className="drink"
       >
         <img
           data-testid="drinks-bottom-btn"
@@ -19,6 +22,7 @@ function Footer() {
       <button
         type="button"
         onClick={ () => history.push('/meals') }
+        className="meal"
       >
         <img
           data-testid="meals-bottom-btn"
