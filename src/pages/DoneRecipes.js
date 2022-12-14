4e4994d72e36103228680aeb2df723d55031af65
Priscila <priscilasartori2@gmail.com>
2022-12-14 13:16:22 -0300

    CSS

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -58,6 +58,7 @@ function DoneRecipes() {
       {
         doneRecipes?.map((done, index) => (
           <div
+            className="recipes"
             key={ done.id }
           >
             <button
@@ -71,55 +72,74 @@ function DoneRecipes() {
                 className="img"
               />
             </button>
-            <button
-              type="button"
-              onClick={ () => history.push(`/${done.type}s/${done.id}`) }
-            >
-              <h1 data-testid={ `${index}-horizontal-name` }>{done.name}</h1>
-            </button>
-            <p data-testid={ `${index}-horizontal-done-date` }>
-              Done in:
-              {' '}
-              {done.doneDate}
-            </p>
-            <button
-              type="button"
-              onClick={ () => {
-                copy(`http://localhost:3000/${done.type}s/${done.id}`);
-                setButtonShare('sim');
-              } }
-            >
-              <img
-                data-testid={ `${index}-horizontal-share-btn` }
-                src={ shareIcon }
-                alt="shareIcon"
-                className="btnShare"
-              />
-            </button>
-            {done.type === 'drink'
-              ? (
-                <p
-                  data-testid={ `${index}-horizontal-top-text` }
-                >
-                  {done.alcoholicOrNot}
-                </p>
-              ) : (
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  {done.nationality}
-                  {' '}
-                  -
-                  {' '}
-                  {done.category}
-                </p>
-              )}
-            {done.tags.map((tag) => (
-              <p
-                key={ tag }
-                data-testid={ `${index}-${tag}-horizontal-tag` }
+            <div className="info">
+              <button
+                type="button"
+                onClick={ () => history.push(`/${done.type}s/${done.id}`) }
               >
-                {tag}
+                <h1
+                  data-testid={ `${index}-horizontal-name` }
+                  className="name"
+                >
+                  {done.name}
+                </h1>
+              </button>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+                className="date"
+              >
+                Done in:
+                {' '}
+                {done.doneDate}
               </p>
-            ))}
+              {done.type === 'drink'
+                ? (
+                  <p
+                    data-testid={ `${index}-horizontal-top-text` }
+                    className="date"
+                  >
+                    {done.alcoholicOrNot}
+                  </p>
+                ) : (
+                  <p
+                    data-testid={ `${index}-horizontal-top-text` }
+                    className="nationality"
+                  >
+                    {done.nationality}
+                    {' '}
+                    -
+                    {' '}
+                    {done.category}
+                  </p>
+                )}
+              <div className="tags">
+                {done.tags.map((tag) => (
+                  <p
+                    key={ tag }
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                    className="tag"
+                  >
+                    {tag}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={ () => {
+                  copy(`http://localhost:3000/${done.type}s/${done.id}`);
+                  setButtonShare('sim');
+                } }
+              >
+                <img
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  src={ shareIcon }
+                  alt="shareIcon"
+                  className="btnShare"
+                />
+              </button>
+            </div>
           </div>
         ))
       }
