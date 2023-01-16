3b9163f55e81c795b2e721592761f8f70498c644
Priscila <priscilasartori2@gmail.com>
2023-01-16 16:24:51 -0300

    Requisitos 20 e 21

---
+++ src/pages/Recipes.js
@@ -0,0 +1,18 @@
+// import PropTypes from 'prop-types';
+// import Meals from './Meals';
+// import Drinks from './Drinks';
+
+// function Recipes({ type }) {
+//   return (
+//     <div>
+//       Recipes
+//       {type === 'meals' ? <Meals /> : <Drinks /> }
+//     </div>
+//   );
+// }
+
+// Recipes.propTypes = {
+//   type: PropTypes.string.isRequired,
+// };
+
+// export default Recipes;
